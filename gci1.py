a871ed580e9a04039387c5b020daa65da571d709
ayushagrawal24 <35636761+ayushagrawal24@users.noreply.github.com>
2018-11-06 17:24:48 +0530

    Create gci1.py

---
+++ gci1.py
@@ -0,0 +1,6 @@
+for i in range(0,10):
+    print("GCI is great")
+
+name = input("Enter your name: ")
+
+print("Hi %s, pleased to meet you! Did you know your name backwards is %s" %(name, name[::-1]))
